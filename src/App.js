dd6813efabd8cbd73b025f1ee92f3ae13160a93d
azeelleeza <leezaluncheon@gmail.com>
2018-05-23 08:56:18 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React, { Component } from 'react';
-import {Grid, Segment} from 'semantic-ui-react';
-import './App.css';
-import MainBox from './containers/MainBox.js'
+import React, { Component } from "react";
+import { Grid, Segment } from "semantic-ui-react";
+import "./App.css";
+import MainBox from "./containers/MainBox.js";
 
 class App extends Component {
   render() {
     return (
       <Grid centered column={1}>
-      <Grid.Row></Grid.Row>
-      <Grid.Row></Grid.Row>
-      <Grid.Row></Grid.Row>
-        <Grid.Row >
+        <Grid.Row>
           <Grid.Column width={10}>
             <Segment>
               <MainBox />
